5efa2aae51eb63e55fc9e53edab1dcade336538e
Bianca Gabriel <bainca.gabriel14@gmail.com>
2022-07-14 16:59:10 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>olá mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
